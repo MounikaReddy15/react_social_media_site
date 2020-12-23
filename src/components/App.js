f14de54dbbe19fd76f3a44fc3ee1cbb7fc0887f4
unknown <mounikareddygonae15@gmail.com>
2020-12-23 18:41:21 +0530

    add login & signup ui

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
-import { Home, Navbar, Page404 } from './';
-
-const Login = () => <div>Login</div>;
-
-const Signup = () => <div>Signup</div>;
+import { Home, Navbar, Page404, Login, Signup } from './';
 
 class App extends React.Component {
   componentDidMount() {
