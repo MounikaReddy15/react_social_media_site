84d7598b737aa3adcc323213314737d7db6bb66c
unknown <mounikareddygonae15@gmail.com>
2020-12-23 20:27:00 +0530

    user loggin - add reducers

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,10 @@
 export const UPDATE_POSTS = 'UPDATE_POSTS';
+
+// login action types
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+// start - disable login btn
+// success - send user and jwt token
+// fail - display the err
